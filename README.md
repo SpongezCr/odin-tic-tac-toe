efd8c8bdef417c5e908dd445aac45c5214ba4b08
SpongezCr <gavinn0000@gmail.com>
2025-07-08 15:08:38 +0800

    Set up all files, skeleton for HTML and CSS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# odin-tic-tac-toe
+This is a project in The Odin Project to make a browser Tic Tac Toe game.
+
+Mainly practises the use of factory functions, modules and closures.
---
+++ script.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+const ticTacToeGame = (() => {
+    let board = [];
+    for (let i = 0; i < 3; i++) 
+        board[i] = new Array(3).fill(null)
+    const player1 = 'O';
+    const player2 = 'X';
+})()
